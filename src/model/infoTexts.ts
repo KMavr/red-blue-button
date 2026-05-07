b9dc2e84c5ba1cc0cb263dabafd62e53fdc00b56
Konstantinos Mavrikas <mavrikas.konstantinos@gmail.com>
2026-05-07 13:39:07 +0300

    Type fix.

--- src/model/infoTexts.ts
+++ src/model/infoTexts.ts
@@ -8,8 +8,7 @@ import PlainList from '../components/PlainList/PlainList';
 import LabeledList from '../components/LabeledList/LabeledList';
 import TransParagraph from '../components/TransParagraph/TransParagraph';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type BlockOf<T extends React.ComponentType<any>> = {
+type BlockOf<T extends React.ComponentType<object>> = {
   component: T;
   props: React.ComponentProps<T>;
 };
